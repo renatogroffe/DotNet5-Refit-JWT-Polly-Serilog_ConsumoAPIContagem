372da0e8bd765ec3802e7527545a42fb0561a5a3
Renato Groffe <renato.groffe@yahoo.com.br>
2021-08-27 19:23:10 -0300

    .NET 5 + Refit + Consumo de API REST de contagem de acessos + JWT + Código Resiliente com Polly + Logging com Serilog
    .NET 5 + Refit + Consumo de API REST de contagem de acessos + JWT + Código Resiliente com Polly + Logging com Serilog

---
+++ ConsumoAPIContagem/Interfaces/IContagemAPI.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+using Refit;
+using ConsumoAPIContagem.Models;
+
+namespace ConsumoAPIContagem.Interfaces
+{
+    public interface IContagemAPI
+    {
+        [Get("/Contador")]
+        Task<ResultadoContador> ObterValorAtualAsync(
+            [Header("Authorization")]string token);       
+    }
+}
---
+++ ConsumoAPIContagem/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using ConsumoAPIContagem.Clients;
+
+namespace ConsumoAPIContagem
+{
+    class Program
+    {
+        private static async Task Interromper()
+        {
+            await Console.Out.WriteLineAsync(
+                "Pressione qualquer tecla para continuar...");
+            Console.ReadKey();
+        }
+
+        static async Task Main()
+        {
+			// Projeto com as APIs utilizadas nos testes:
+			// https://github.com/renatogroffe/ASPNETCore5-REST_API-JWT-Swagger_ContagemAcessos
+            var builder = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile($"appsettings.json");
+            var config = builder.Build();
+
+
+            var logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            var apiContagemClient =
+                    new APIContagemClient(config, logger);
+            await apiContagemClient.Autenticar();
+            while (true)
+            {
+                await apiContagemClient.ExibirResultadoContador();
+                await Interromper();
+            }
+        }
+    }
+}
